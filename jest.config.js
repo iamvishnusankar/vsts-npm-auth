6e1cb400734652df1e54c73f638c102e6eca2b72
Vishnu Sankar <4602725+iamvishnusankar@users.noreply.github.com>
2020-08-15 00:01:38 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ...require('@corex/jest/reporter'),
+  preset: 'ts-jest',
+  projects: ['packages/*'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+}
